f2c72052f189634fac2eebf8086d76f4e58dd3ee
Lino <lgustavo@enjoei.com.br>
2020-02-25 14:46:10 -0300

    $commit_message

---
+++ maintenance.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# say hello because it's polite
+echo "hi! this is > lv-cli < 's mantenance script"
+
+## available commands
+publish(){
+    
+    while true; do
+
+        if [[ `git status --porcelain` ]]; then
+            echo "you do have local changes!"
+            echo "will now rebuild, commit and publish."
+        else
+            echo "you don't have local changes. do you really need a new version?"
+            echo "anyway, will now rebuild, commit (because of /lib) and publish."
+        fi
+        
+        read -p "it's that okay (y/n)? " yn; echo "--"
+
+        case $yn in
+            [Yy]* ) 
+
+                echo "publishing a new npm version. what's the commit message?"
+                read commit_message; echo "--"
+
+                npm run rebuild
+                git add .
+                git commit --allow-empty -m '$commit_message'
+                npm version patch -m 'npm version bump'
+                npm publish
+                echo "there you go!"
+
+                break;;
+            [Nn]* )
+                echo "ok! bye."
+                exit;;
+            * ) 
+                echo "Please answer yes or no."
+        esac
+    done
+}
+
+## what should we do?
+
+ while true; do
+
+        echo "available options are: publish (p)."
+        read -p "choose one: " opt; echo "--"
+
+        case $opt in
+            [Pp]* ) publish; break;;
+            * ) echo "ok! bye."; exit;;
+        esac
+done
+
